ef00fbcb3844fa4d81a8d43b1e551847c34e72e2
yuu0252 <129631633+yuu0252@users.noreply.github.com>
2023-10-22 10:52:55 +0900

    Update EditUser.tsx

--- src/EditUser.tsx
+++ src/EditUser.tsx
@@ -7,7 +7,7 @@ export const EditUser: React.FC = () => {
       .get(`${URL}/users`, {
         headers: {
           authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTc5MjA1MzAsImlhdCI6MTY5NzgzNDEzMCwic3ViIjoiNTQ1NDY1NTczNTQiLCJ1c2VyX2lkIjoiNWE4Y2NhYWItN2VmNS00YjhlLWFkYWUtMGJlNWZiNGMyZTNlIn0.-I9Mn7GbHJd5da4ojU9gfA5p7zzPpvfYSN6mtw5d4dI",
+            "Bearer ",
         },
       })
       .then((res) => {
